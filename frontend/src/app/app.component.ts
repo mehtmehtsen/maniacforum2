650101d7b9324514fcea0cff67fe95748df884b9
Marius Tippkämper <tippkaemper@posteo.de>
2021-09-14 22:27:19 +0200

    show board titles in angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardResponse } from './services/api/models';
 import { ApiService } from './services/api/services';
 
 @Component({
@@ -8,12 +9,13 @@ import { ApiService } from './services/api/services';
 })
 export class AppComponent implements OnInit {
   title = 'maniacforum2';
+  boards: Array<BoardResponse> = [];
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
     this.apiService.getBoards().subscribe((r) => {
-      console.log(r);
+      this.boards = r;
     });
   }
 }
