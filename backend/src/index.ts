40e34974cacf2b3ce5b58e0d86ca2ce6f1281c4f
Marius Tippkämper <tippkaemper@posteo.de>
2021-09-14 21:21:18 +0200

    remove stuff

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,3 @@
-// "use strict";
-
-// const express = require("express");
-
-// // App
-// const app = express();
-// app.listen(PORT, HOST);
-
-// app.get("/", (req, res) => {
-//   pg.many("SELECT * FROM boards")
-//     .then((data) => {
-//       const out = JSON.stringify(data);
-//       console.log(out);
-//       res.json(out);
-//     })
-//     .catch((error) => {
-//       const out = JSON.stringify(error);
-//       console.log(out);
-//       res.json(out);
-//     });
-// });
-
 import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
