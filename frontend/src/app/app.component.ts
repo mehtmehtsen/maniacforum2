d3e44b381f24ec795d4e6eda7889bf8f3822952e
Marius Tippkämper <tippkaemper@posteo.de>
2021-09-16 23:08:37 +0200

    rm logs

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -14,9 +14,7 @@ export class AppComponent implements OnInit {
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    console.log('%c getting boards', 'background: #222;');
     this.apiService.getBoards().subscribe((r) => {
-      console.log('%c theres boards', 'background: #222;', r);
       this.boards = r;
     });
   }
