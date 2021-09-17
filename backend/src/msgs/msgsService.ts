dc766f479533c8e0e619bd44ef4e055c93d5885c
Marius Tippkämper <tippkaemper@posteo.de>
2021-09-17 21:53:39 +0200

    get thread/msgs using ltree

--- backend/src/msgs/msgsService.ts
+++ backend/src/msgs/msgsService.ts
@@ -17,42 +17,7 @@ export class MsgsService {
   public async getMsgs(threadId: number): Promise<Msg[]> {
     let out: Msg[] = [];
     await pg
-      .many(
-        `WITH RECURSIVE msgtree AS (
-          SELECT
-            board_id,
-            id,
-            user_id,
-            parent_id,
-            parent_user_id,
-            timestamp,
-            subject,
-            body,
-            author_mod
-          FROM
-            msgs
-          WHERE
-            id = $1 
-          UNION
-          SELECT
-            m.board_id,
-            m.id,
-            m.user_id,
-            m.parent_id,
-            m.parent_user_id,
-            m.timestamp,
-            m.subject,
-            m.body,
-            m.author_mod
-          FROM
-            msgs m
-          INNER JOIN msgtree t ON m.parent_id = t.id
-        ) SELECT
-          *
-        FROM
-          msgtree;`,
-        threadId
-      )
+      .many(`SELECT * FROM msgs WHERE $1::text::ltree @> path`, threadId)
       .then((data: Msg[]) => (out = data))
       .catch((error) => console.error(error));
     return out;
