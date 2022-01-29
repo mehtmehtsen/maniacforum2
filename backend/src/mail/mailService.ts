9bcdac8ddf256c4b5ca830d9509334ca654982ae
Marius Tippkämper <tippkaemper@posteo.de>
2022-01-29 23:47:38 +0100

    send mail via posteo

--- backend/src/mail/mailService.ts
+++ backend/src/mail/mailService.ts
@@ -1,7 +1,33 @@
+import { Transport } from "nodemailer";
 import nodemailer from "./../../node_modules/nodemailer";
 
 export class MailService {
+  transport = nodemailer.createTransport({
+    host: "posteo.de",
+    port: 465,
+    secure: true, // upgrade later with STARTTLS
+    auth: {
+      user: "USER",
+      pass: "YOUR_PWD",
+    },
+  });
+
   public async send(address: string, subject: string, body: string) {
     console.log("sending", address, subject, body);
+
+    const mailOptions = {
+      from: "tippkaemper@posteo.de",
+      to: address,
+      subject: "Registering in Maniacforum2",
+      text: body,
+    };
+
+    this.transport.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
   }
 }
