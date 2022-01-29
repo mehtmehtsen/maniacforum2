2de2ffdc74635a966be3037aa1a8f476d81f0674
Marius Tippkämper <tippkaemper@posteo.de>
2022-01-29 23:23:55 +0100

    modify signup validation order

--- backend/src/signup/signupService.ts
+++ backend/src/signup/signupService.ts
@@ -12,14 +12,6 @@ export class SignupService {
   ): Promise<void> {
     let invalid = false;
 
-    // check for password too short
-    if (password.length < 7) {
-      invalid = true;
-
-      return validationErrorResponse(422, { reason: "Password is too short." });
-    }
-    if (invalid) return;
-
     // check for valid email address
     const emailRegex =
       /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -57,6 +49,14 @@ export class SignupService {
           });
         }
       });
+    if (invalid) return;
+
+    // check for password too short
+    if (password.length < 7) {
+      invalid = true;
+
+      return validationErrorResponse(422, { reason: "Password is too short." });
+    }
     if (invalid) return;
 
     // save data in db
