375c0b2e25d4a60958d63ea365fba30ef9ebf09a
Marius Tippkämper <tippkaemper@posteo.de>
2022-01-29 23:23:30 +0100

    workaround bug where invalid users where still saved

--- backend/src/signup/signupService.ts
+++ backend/src/signup/signupService.ts
@@ -10,42 +10,54 @@ export class SignupService {
     password: string,
     validationErrorResponse: TsoaResponse<422, { reason: string }>
   ): Promise<void> {
+    let invalid = false;
+
     // check for password too short
     if (password.length < 7) {
+      invalid = true;
+
       return validationErrorResponse(422, { reason: "Password is too short." });
     }
+    if (invalid) return;
 
     // check for valid email address
     const emailRegex =
       /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
     if (!emailRegex.test(email)) {
+      invalid = true;
       return validationErrorResponse(422, {
         reason: "This is not an email address!1",
       });
     }
+    if (invalid) return;
 
     // check for email address already used
     await pg
       .manyOrNone(`SELECT email FROM users WHERE email=$1`, email)
-      .then(async (r) => {
+      .then((r) => {
         if (r.length > 0) {
+          invalid = true;
+
           return validationErrorResponse(422, {
             reason: "There already is an account with that email address.",
           });
         }
       });
+    if (invalid) return;
 
     // check for username already used
     await pg
       .manyOrNone(`SELECT username FROM users WHERE username=$1`, username)
       .then((r) => {
         if (r.length > 0) {
+          invalid = true;
           return validationErrorResponse(422, {
             reason: "There already is an account with that username.",
           });
         }
       });
+    if (invalid) return;
 
     // save data in db
     const hashedPassword = hashSync(password, 10);
@@ -69,8 +81,6 @@ export class SignupService {
             ($1, $2, $3)`,
           [r.id, token, new Date()]
         ).then(() => {
-          console.log("now send mail with token");
-
           const mailService = new MailService();
           mailService.send(email, "mup", `mip ${token}`);
         });
