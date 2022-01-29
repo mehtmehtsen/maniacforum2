435ca480d236921046ca960915004630b371cf33
Marius Tippkämper <tippkaemper@posteo.de>
2022-01-30 00:39:39 +0100

    register

--- frontend/src/app/user/registration/registration.component.ts
+++ frontend/src/app/user/registration/registration.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ApiService } from 'src/app/services/api/services';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  @ViewChild('mail') mailInput!: ElementRef;
+  @ViewChild('user') userInput!: ElementRef;
+  @ViewChild('pass') passInput!: ElementRef;
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  async handleSubmit(): Promise<void> {
+    const email = (this.mailInput.nativeElement as HTMLInputElement).value;
+    const username = (this.userInput.nativeElement as HTMLInputElement).value;
+    const password = (this.passInput.nativeElement as HTMLInputElement).value;
+
+    await this.apiService
+      .signup({ email, username, password })
+      .subscribe((r) => {
+        console.log(r);
+      });
   }
-
 }
