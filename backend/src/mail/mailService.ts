50341bf59cf6a9be3bd4955ea16f6b0b1e2f6467
Marius Tippkämper <tippkaemper@posteo.de>
2022-01-29 23:24:10 +0100

    npm i nodemailer and types

--- backend/src/mail/mailService.ts
+++ backend/src/mail/mailService.ts
@@ -1,3 +1,5 @@
+import nodemailer from "./../../node_modules/nodemailer";
+
 export class MailService {
   public async send(address: string, subject: string, body: string) {
     console.log("sending", address, subject, body);
