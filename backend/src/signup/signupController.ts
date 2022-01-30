920cb0d83e81fa0e2a0d9f701dce801e337a8d7d
Marius Tippkämper <tippkaemper@posteo.de>
2022-01-30 17:07:40 +0100

    add documentation to signupController

--- backend/src/signup/signupController.ts
+++ backend/src/signup/signupController.ts
@@ -15,8 +15,11 @@ import { SignupService } from "./signupService";
 @Route("signup")
 export class SignupController extends Controller {
   /**
-   *
-   *
+   * Initial sign up for new users. User will have to verify email address
+   * by clicking the link that got sent to it.
+   * @param email user's email address. Mustn't be present in DB for it to be accepted.
+   * @param username desired username. Must be 3-16 chars and not already present in DB
+   * @param password password. Minimum length of 7 chars.
    */
   @Post()
   public async signup(
